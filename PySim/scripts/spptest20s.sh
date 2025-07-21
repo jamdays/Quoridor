925b2e9d635ad798be9c6779ce0162091d755ac8
Djaya Maximilian <djayamax@teach.cs.toronto.edu>
2025-07-21 19:09:53 -0400

    finished some more testing 10 long switched left, 50 switched 20s left

---
+++ PySim/scripts/spptest20s.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /h/u13/c3/01/djayamax/Documents/Quoridor/PySim
+{ echo "n";
+ echo 5;
+ echo y;
+ echo "logs/20s_p1_lower_spp/log$1.txt"
+ echo y;
+ echo .9;
+ echo .9;
+ echo .5;
+ echo .9;
+ echo p;
+ echo p;
+ echo 20;
+ echo 20;
+ echo w;
+ echo w;
+ echo n;
+ echo n;
+ cat;
+}| python3 sim.py
